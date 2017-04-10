fed351408965ec2a1c0a610b693ee598d30bded7
pbcquoc <pbcquoc@gmail.com>
2017-04-10 23:42:17 +0700

    seq2seq

--- lstm.py
+++ lstm.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
-def lstm_cell(i, m, state, vocab_size, mem_size, scope):
+def lstm_cell(i, m, state, embedding_size, mem_size, scope):
   with tf.variable_scope(scope):
-    ifcox = tf.get_variable(name='ifcox', shape=[vocab_size, 4*mem_size],initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
-    ifcom = tf.get_variable(name='ifcom', shape=[mem_size, 4*mem_size],  initializer=tf.truncated_noraml_initializer(mean=0.0, stddev=0.1))
+    ifcox = tf.get_variable(name='ifcox', shape=[embedding_size, 4*mem_size],initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+    ifcom = tf.get_variable(name='ifcom', shape=[mem_size, 4*mem_size],  initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
     ifcob = tf.get_variable(name='ifcob', shape=[4*mem_size], initializer=tf.constant_initializer(0))
   
     all_gates = tf.matmul(i, ifcox) + tf.matmul(m, ifcom) + ifcob
@@ -16,4 +16,11 @@ def lstm_cell(i, m, state, vocab_size, mem_size, scope):
     
     return out_gate*tf.tanh(state), state
 
+def linear(x, shape, activation_fn, scope):
+  with tf.variable_scope(scope):
+    W = tf.get_variable(name='W', shape=shape, initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+    b = tf.get_variable(name='b', shape=shape[-1], initializer=tf.constant_initializer(0))
+    h = tf.matmul(x, W) + b
+    a = activation_fn(h)
 
+  return a
--- seq2seq.py
+++ seq2seq.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import json
-from lstm import lstm_cell
+from lstm import lstm_cell, linear
 
 questions = np.load('data/questions.npy')
 answers = np.load('data/answers.npy')
@@ -13,28 +13,57 @@ embedding_size = 256
 mem_size = 512
 batch_size = 16
 
-question = tf.placeholder(tf.int32, shape=[None, encoder_unrollings])
-answer = tf.placeholder(tf.int32, shape=[None, decoder_unrollings])
+question = tf.placeholder(tf.int32, shape=[batch_size, encoder_unrollings])
+answer = tf.placeholder(tf.int32, shape=[batch_size, decoder_unrollings])
+answer_x = answer[:, :-1]
+answer_y = tf.reshape(answer[:, 1:], [-1])
 
-def encoder():
+embedded = tf.get_variable(name='embedded', shape=[vocab_size, embedding_size], initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+
+def encoder(cx):
   outputs = []
-  with tf.variable_scope('encoder'):
-
-    embedded = tf.get_variable(name='embedded', shape=[vocab_size, embedding_size], initializer=tf.truncated_normal(mean=0.0, stddev=0.1))
-    x = tf.nn.embedding_lookup(embedded, question) # batch_size x unrolling x embedding_size
-    mem = tf.constant(0, shape=[tf.shape(x)[0], mem_size], name='initial memory')
-    state = tf.constant(0, shape=[tf.shape(x)[0], mem_size], name='initial state')
+  with tf.variable_scope('encoder') as scope:
+    x = tf.nn.embedding_lookup(embedded, cx) # batch_size x unrolling x embedding_size
+    mem = tf.constant(0.0, shape=[batch_size, mem_size], name='initial_memory')
+    state = tf.constant(0.0, shape=[batch_size, mem_size], name='initial_state')
 
     for i in range(encoder_unrollings):
-      mem, state = lstm_cell(x[:, i,:], mem, state, vocab_size, mem_size, 'lstm')
-      outputs.append(output)
+      mem, state = lstm_cell(x[:, i, :], mem, state, embedding_size, mem_size, 'lstm')
+      scope.reuse_variables()
+      outputs.append(mem)
 
   return outputs, state
 
-def decoder(mem, state):
-  with tf.variable_scope('decoder'):
-    embedded = tf.get_variable(name='embedded', shape=)
-      
+def decoder(cx, mem, state):
+  outputs = []
+  with tf.variable_scope('decoder') as scope:
+    x = tf.nn.embedding_lookup(embedded, cx) 
     
+    for i in range(decoder_unrollings - 1):
+      mem, state = lstm_cell(x[:, i, :], mem, state, embedding_size, mem_size, 'lstm')
+      scope.reuse_variables()
+      outputs.append(mem)
 
+  return outputs, state
 
+mems_encoder, state_encoder = encoder(question)
+mems_decoder, _ = decoder(answer_x, mems_encoder[-1], state_encoder)
+logits = tf.concat(values=mems_decoder, axis=0)
+ys = linear(logits, shape=[mem_size, vocab_size], activation_fn=tf.identity, scope='linear')
+total_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=ys, labels=answer_y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-2).minimize(total_loss)
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+idx_q = np.load('data/questions.npy')
+idx_a = np.load('data/answers.npy')
+print idx_q.shape, idx_a.shape
+for i in xrange(1, 1000):
+  for batch in xrange(len(idx_q)/batch_size):
+    batch_qs = idx_q[batch*batch_size:(batch+1)*batch_size]
+    batch_as = idx_a[batch*batch_size:(batch+1)*batch_size]
+
+    _, loss = sess.run([optimizer, total_loss], feed_dict={question:batch_qs, answer:batch_as})
+  print loss
