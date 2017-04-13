2a1d931e89bd9803bab19a13f475454630ef91eb
pbcquoc <pbcquoc@gmail.com>
2017-04-13 17:24:09 +0700

    multinomial sampling

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 # Overview
   The input is twitter chat that contains questions and their responsed answers. I clean all messy characters.
-  In training state, 
+  In training state, we forward questions and try to predict answers. The model is followed by
+  ![GitHub Logo](http://suriyadeepan.github.io/img/seq2seq/seq2seq2.png)
 
 # Dependencies
   * Python 2.7
   * Tensorflow 1.xx
-  I only test it on Ubuntu 16.04
+  * I only test it on Ubuntu 16.04
 
 # Training
+
+# Evaluate
--- seq2seq.py
+++ seq2seq.py
@@ -14,7 +14,7 @@ PAD = w2idx['_']
 encoder_unrollings, decoder_unrollings  = 20, 22
 embedding_size = 256
 mem_size = 512
-batch_size = 16
+batch_size = 128
 
 
 question = tf.placeholder(tf.int32, shape=[batch_size, encoder_unrollings])
@@ -64,10 +64,9 @@ def max_sampling(max_length):
     for _ in range(max_length):
       mem_decoder, state_decoder = decoder(x, mem_decoder[-1], state_decoder, 1, decoder_reuse=True)
       _y = linear(mem_decoder[0], shape=[mem_size, vocab_size], activation_fn=tf.identity, scope='linear', reuse=True)
-      y = tf.argmax(_y, axis=1)
-
-      x = tf.reshape(y, [-1, 1])
-      ys.append(y)
+      x = tf.multinomial(_y, 1)
+      #x = tf.reshape(y, [-1, 1])
+      ys.append(tf.reshape(x, [-1]))
 
   return ys
 
@@ -112,9 +111,9 @@ for i in xrange(1, 1000):
     _, loss = sess.run([optimizer, total_loss], feed_dict={question:batch_qs, answer:batch_as})
     if n % 100 == 0:
       ys_sampling = sess.run(sampling, feed_dict={question: idx_q_sample})
-      for y_sampling in np.transpose(ys_sampling):
+      for y_sampling in np.transpose(ys_sampling)[:10]:
         a_sampling = utils.idxs2str(y_sampling, idx2w)
-        print a_sampling, '\n'
+        print a_sampling
 
       print 'Iter', n, ': ', loss
     
